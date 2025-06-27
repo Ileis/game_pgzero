28a4783e36265bc9ab92393323afbdd9e84d4d59
Ileis <gabrielileis07@gmail.com>
2025-06-27 11:46:18 -0300

    animation fixed

--- src/Player.py
+++ src/Player.py
@@ -26,39 +26,34 @@ class Player(Entity):
 
     def walk_left(self):
         self.x -= self._speed
-        self._staff.pos = self.pos
         self._norm_player()
         self._direction = LEFT
-        self._fix_direction()
 
     def walk_right(self):
         self.x += self._speed
-        self._staff.pos = self.pos
         self._norm_player()
         self._direction = RIGHT
-        self._fix_direction()
 
     def jump(self):
         if not self._jumping:
             self._jumping = True
             self._vel_y = self._jump_strength
-            self.image = f'player_jumping_{self._direction}'
         else:
             self._vel_y += GRAVITY
             self.y += self._vel_y
-            self._staff.pos = self.pos
 
             if self.y >= FLOOR - self.height / 2:
                 self.y = FLOOR - PLAYER_HIGHT / 2
                 self._vel_y = 0
                 self._jumping = False
-                self._animate()
 
     def draw(self):
         super().draw()
         self._staff.draw()
 
     def update(self):
+        self._fix_image_player_direction_action()
+
         if self.keyboard.space or self._jumping:
             self.jump()
 
@@ -75,13 +70,15 @@ class Player(Entity):
         self.x = max(self.width / 2, min(WIDTH - self.width / 2, self.x))
     
     def _animate(self):
-        if not self._jumping:
-            self._frame_img = 1 if self._frame_img == 2 else 2
-            self._fix_direction()
-            self._staff.pos = self.pos
-            self.clock.schedule_unique(self._animate, FRAME_RATE)
+        self._frame_img = 1 if self._frame_img == 2 else 2
+        self.clock.schedule_unique(self._animate, FRAME_RATE)
 
-    def _fix_direction(self):
+    def _fix_staff_pos(self):
+        self._staff.pos = self.pos
+
+    def _fix_image_player_direction_action(self):
+        self._fix_staff_pos()
+
         if self._jumping:
             self.image = f'player_jumping_{self._direction}'
         else:
---
+++ src/Projectile.py
@@ -0,0 +1,4 @@
+from Entity import Entity
+
+class Projectile(Entity):
+    pass
